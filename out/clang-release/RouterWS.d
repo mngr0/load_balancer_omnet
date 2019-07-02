60d1794699cc595399501281c9b1d4f68d76dab4
mngr0 <ngrmarco@tutanota.com>
2019-07-02 10:51:26 +0200

    ms

--- out/clang-release/RouterWS.d
+++ out/clang-release/RouterWS.d
@@ -1,11 +1,11 @@
 out/clang-release//RouterWS.o: RouterWS.cc \
-  /home/marco/omnetpp-5.4.1/include/omnetpp.h RouterWS.h \
+  /home/marco/omnetpp-5.4.1BIS/include/omnetpp.h RouterWS.h \
   ../queueinglib/Router.h ../queueinglib/QueueingDefs.h \
   ../queueinglib/PassiveQueue.h ../queueinglib/IPassiveQueue.h \
   ../queueinglib/SelectionStrategies.h ../queueinglib/Job.h \
-  ../queueinglib/Job_m.h
+  ../queueinglib/Job_m.h SelectionStrategiesInOut.h
 
-/home/marco/omnetpp-5.4.1/include/omnetpp.h:
+/home/marco/omnetpp-5.4.1BIS/include/omnetpp.h:
 
 RouterWS.h:
 
@@ -22,3 +22,5 @@ RouterWS.h:
 ../queueinglib/Job.h:
 
 ../queueinglib/Job_m.h:
+
+SelectionStrategiesInOut.h:
---
+++ out/clang-release/SelectionStrategiesInOut.d
@@ -0,0 +1,27 @@
+out/clang-release//SelectionStrategiesInOut.o: \
+  SelectionStrategiesInOut.cc \
+  /home/marco/omnetpp-5.4.1BIS/include/omnetpp.h \
+  ../queueinglib/PassiveQueue.h ../queueinglib/QueueingDefs.h \
+  ../queueinglib/IPassiveQueue.h ../queueinglib/SelectionStrategies.h \
+  ../queueinglib/Job.h ../queueinglib/Job_m.h ../queueinglib/Server.h \
+  ../queueinglib/IServer.h SelectionStrategiesInOut.h
+
+/home/marco/omnetpp-5.4.1BIS/include/omnetpp.h:
+
+../queueinglib/PassiveQueue.h:
+
+../queueinglib/QueueingDefs.h:
+
+../queueinglib/IPassiveQueue.h:
+
+../queueinglib/SelectionStrategies.h:
+
+../queueinglib/Job.h:
+
+../queueinglib/Job_m.h:
+
+../queueinglib/Server.h:
+
+../queueinglib/IServer.h:
+
+SelectionStrategiesInOut.h:
